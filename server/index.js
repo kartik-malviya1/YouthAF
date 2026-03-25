c7d2106baf61452f10a1f8934a0fd6dd0d23220a
kartik-malviya <work.kartik.malviya@gmail.com>
2026-03-25 12:37:05 +0530

    feat: configure CORS

--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ dotenv.config();
 
 const app = express();
 connectDB();
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
